b3a6c17dd2910e6de1de821fa626d6a528e8424c
pkd11011 <phamdo1132@gmail.com>
2026-03-22 17:50:35 +0700

    part1

---
+++ Assets/Scripts/Blinking.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+public class Blinking : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    void Start() { spriteRenderer = GetComponent<SpriteRenderer>(); } // Lấy thành phần vẽ [cite: 504]
+    void Update() { spriteRenderer.enabled = !spriteRenderer.enabled; } // Bật/tắt liên tục [cite: 529]
+}
---
+++ Assets/Scripts/ShowLog.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+public class ShowLog : MonoBehaviour
+{
+    void Start() { Debug.Log("Hello World!"); } // Chạy 1 lần [cite: 452, 454]
+    void Update() { Debug.Log("Update gọi tại khung hình: " + Time.frameCount); } // Chạy liên tục [cite: 467, 469]
+}
