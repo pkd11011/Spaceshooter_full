8e7cbd9459a48b46b494e996cc22e5bb9e205f12
pkd11011 <phamdo1132@gmail.com>
2026-03-22 19:05:29 +0700

    part 2

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float flySpeed; // Tốc độ bay của đạn
+
+    void Update()
+    {
+        // Tính toán vị trí mới dựa trên tốc độ và thời gian
+        var newPosition = transform.position;
+        newPosition.y += Time.deltaTime * flySpeed;
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bulletPrefabs;
+    public Transform muzzlePoint;
+    public float shootingInterval = 0.1f;
+    private float lastBulletTime;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time - lastBulletTime > shootingInterval)
+            {
+                ShootBullet();
+                lastBulletTime = Time.time;
+            }
+        }
+    }
+
+    private void ShootBullet()
+    {
+        Instantiate(bulletPrefabs, muzzlePoint.position, muzzlePoint.rotation);
+    }
+}
